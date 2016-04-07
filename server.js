c3088bd1990a8cbe97e8e685bac95938c0218865
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-04-07 02:33:29 -0700

    Fixed url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var parser = {
 
 // Database Connection
 var mongoose = require('mongoose');
- mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/cogs121');
+ mongoose.connect('mongodb://127.0.0.1/cogs121' || process.env.MONGOLAB_URI);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongo DB Connection Error:'));
 db.once('open', function(callback) {
